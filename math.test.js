6f318c053f33154978a611b11c2608284a2d1919
matteo <matteo.box03@gmail.com>
2025-04-29 17:36:02 +0200

    update test

--- math.test.js
+++ math.test.js
@@ -1,3 +1,4 @@
+const { default: test } = require('node:test');
 const sum = require('./math.js');
 
 test('add 2 and 5 to make 7', () => {
@@ -9,3 +10,17 @@ test('add 3 and 5 to make 8', () => {
     expect(sum(3, 5)).toBe(8);
 }
 );  
+
+test('subtract 5 from 10 to make 5', () => {
+    expect(sum(10, -5)).toBe(5);
+}
+);
+test('multiply 2 and 5 to make 10', () => {
+    expect(sum(2, 5)).toBe(10);
+}
+);
+test('divide 10 by 2 to make 5', () => {
+    expect(sum(10, 2)).toBe(5);
+}
+);
+
