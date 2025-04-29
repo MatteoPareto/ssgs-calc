9e5919a11f7ae88cf9986885d4a8033784147b10
matteo <matteo.box03@gmail.com>
2025-04-29 17:24:23 +0200

    test

--- math.test.js
+++ math.test.js
@@ -5,7 +5,7 @@ test('add 2 and 5 to make 7', () => {
 }
 );  
 
-test('add 3 and 5 to make 7', () => {
-    expect(sum(3, 5)).toBe(7);
+test('add 3 and 5 to make 8', () => {
+    expect(sum(3, 5)).toBe(8);
 }
 );  
