bf23455d857cd23c06257eb674d2a9b10db5b08e
matteo <matteo.box03@gmail.com>
2025-04-29 17:38:13 +0200

    update test

--- math.test.js
+++ math.test.js
@@ -1,26 +1,22 @@
-const { default: test } = require('node:test');
-const sum = require('./math.js');
+const { sum, subtract, multiply, divide } = require('./math.js');
 
 test('add 2 and 5 to make 7', () => {
     expect(sum(2, 5)).toBe(7);
-}
-);  
-
-test('add 3 and 5 to make 8', () => {
-    expect(sum(3, 5)).toBe(8);
-}
-);  
+});
 
 test('subtract 5 from 10 to make 5', () => {
-    expect(sum(10, -5)).toBe(5);
-}
-);
+    expect(subtract(10, 5)).toBe(5);
+});
+
 test('multiply 2 and 5 to make 10', () => {
-    expect(sum(2, 5)).toBe(10);
-}
-);
+    expect(multiply(2, 5)).toBe(10);
+});
+
 test('divide 10 by 2 to make 5', () => {
-    expect(sum(10, 2)).toBe(5);
-}
-);
+    expect(divide(10, 2)).toBe(5);
+});
+
+test('division by zero throws an error', () => {
+    expect(() => divide(10, 0)).toThrow('Division by zero is not allowed');
+});
 
